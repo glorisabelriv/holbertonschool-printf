42fe376094f8537a82e4be5a75c52ad2d26fb5bd
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@airdeglorisabel.lan>
2024-03-20 18:27:04 -0400

    file the function _printf

---
+++ _printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * _printf - Prints formatted output to the standard output
+ * @format: The format string containing format specifiers.
+ * Return: The number of characters printed or -1 if the format string is NULL
+ */
+int _printf(const char *format, ...)
+{
+	va_list argumentList;
+	int i = 0, characterCount = 0;
+	int (*formatFunctionPointer)(va_list) = NULL;
+
+	va_start(argumentList, format);
+	if (format == NULL)
+		return (-1);
+	while (format[i])
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			i++;
+			characterCount++;
+			continue;
+		}
+		i++;
+		formatFunctionPointer = find_format_function(format[i]);
+		if (formatFunctionPointer != NULL)
+		{
+			characterCount += formatFunctionPointer(argumentList);
+		}
+		else
+		{
+			if (format[i] == '\0')
+				return (-1);
+			if (format[i] == '%')
+			{
+				characterCount += _putchar('%');
+			}
+			else
+			{
+				characterCount += _putchar('%');
+				characterCount += _putchar(format[i]);
+			}
+		}
+		i++;
+	}
+	va_end(argumentList);
+	return (characterCount);
+}
