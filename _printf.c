a252f7762023e857787a4e535bf1467a28150561
Julio <julio.1515.pr@gmail.com>
2024-03-21 21:17:48 -0400

    testing merge error

--- _printf.c
+++ _printf.c
@@ -1,49 +1,51 @@
 #include "main.h"
 /**
- * _printf - Prints formatted output to the standard output
- * @format: The format string containing format specifiers.
- * Return: The number of characters printed or -1 if the format string is NULL
+ * _printf - mimic the standard printf function
+ * @...: list of arguments passed to the function
+ * @format: pointer to string
+ *
+ * Return: length of the string
  */
 int _printf(const char *format, ...)
 {
-	va_list argumentList;
-	int i = 0, characterCount = 0;
-	int (*formatFunctionPointer)(va_list) = NULL;
+	va_list args;
+	int count = 0, i = 0, j = 0;
+	spec_t spec_list[] = {{'c', spec_char}, {'s', spec_str}, {'%', spec_percent},
+		{'d', spec_deci}, {'i', spec_deci}, {'\0', NULL}};
 
-	va_start(argumentList, format);
+	va_start(args, format);
 	if (format == NULL)
 		return (-1);
-	while (format[i])
+	while (format != NULL && format[i] != '\0')
 	{
-		if (format[i] != '%')
+		if (format[i] == '%')
 		{
-			_putchar(format[i]);
+			if (format[i + 1] == '\0')
+				return (-1);
+			while (spec_list[j].c != '\0')
+			{
+				if (format[i + 1] == spec_list[j].c)
+				{
+					count += spec_list[j].spec(args);
+					break;
+				}
+				j++;
+			}
+			if (spec_list[j].c == '\0')
+			{
+				count += _putchar(format[i]);
+				count += _putchar(format[i + 1]);
+			}
+			j = 0;
 			i++;
-			characterCount++;
-			continue;
-		}
-		i++;
-		formatFunctionPointer = find_format_function(format[i]);
-		if (formatFunctionPointer != NULL)
-		{
-			characterCount += formatFunctionPointer(argumentList);
 		}
 		else
 		{
-			if (format[i] == '\0')
-				return (-1);
-			if (format[i] == '%')
-			{
-				characterCount += _putchar('%');
-			}
-			else
-			{
-				characterCount += _putchar('%');
-				characterCount += _putchar(format[i]);
-			}
+			_putchar(format[i]);
+			count++;
 		}
 		i++;
 	}
-	va_end(argumentList);
-	return (characterCount);
+	va_end(args);
+	return (count);
 }
